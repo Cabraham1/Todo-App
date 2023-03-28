642e878814e7b7486664a2d4536eebbd8a6aea34
cabraham1 <christopherabraham8@gmail.com>
2023-03-28 18:16:46 +0100

    Add header componetes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header.js';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="container">
+      <div className='app-wrapper'>
+        <div>
+          <Header />
+        </div>
+     </div>
     </div>
   );
 }
